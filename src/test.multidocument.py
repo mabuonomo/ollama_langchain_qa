114d1ff9c92586376a0a2982e56b13822043ad7a
Mario <ma.buonomo@gmail.com>
2024-01-12 16:13:01 +0100

    Update docker-compose.yml and test.multidocument.py

--- src/test.multidocument.py
+++ src/test.multidocument.py
@@ -13,10 +13,13 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
 from langchain.chains import ConversationalRetrievalChain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains import RetrievalQA
 
 ollama_url = os.getenv("OLLAMA_URL", "http://ollama_chat:11434")
 ollama_model = os.getenv("OLLAMA_MODEL", "mistral")
 vectordb_path = os.getenv("VECTORDB_PATH", ".data")
+ollama_obj = Ollama(base_url=ollama_url, model=ollama_model)
+ollama_embeddings = OllamaEmbeddings(base_url=ollama_url, model=ollama_model)
 
 print(f"Using Ollama at {ollama_url} with model {ollama_model}")
 
@@ -43,24 +46,20 @@ print(f"Loaded {len(documents)} documents")
 
 vectordb = Chroma.from_documents(
     documents, 
-    embedding=OllamaEmbeddings(
-        base_url=ollama_url,
-        model=ollama_model
-    ), 
+    embedding=ollama_embeddings, 
     persist_directory=vectordb_path)
 vectordb.persist()
 
 print(f"Created vectorstore")
 
-pdf_qa = ConversationalRetrievalChain.from_llm(
-    Ollama(
-        base_url=ollama_url,
-        model=ollama_model
-        ),
-    vectordb.as_retriever(search_kwargs={'k': 6}),
-    return_source_documents=True,
-    verbose=True
-)
+# pdf_qa = ConversationalRetrievalChain.from_llm(
+#     ollama_obj,
+#     vectordb.as_retriever(search_kwargs={'k': 6}),
+#     return_source_documents=True,
+#     verbose=True
+# )
+
+qachain=RetrievalQA.from_chain_type(ollama_obj, retriever=vectordb.as_retriever())
 
 print(f"Created QA chain")
 
@@ -68,7 +67,7 @@ yellow = "\033[0;33m"
 green = "\033[0;32m"
 white = "\033[0;39m"
 
-chat_history = []
+# chat_history = []
 print(f"{yellow}---------------------------------------------------------------------------------")
 print('Welcome to the DocBot. You are now ready to start interacting with your documents')
 print('---------------------------------------------------------------------------------')
@@ -80,11 +79,14 @@ while True:
     if query == '':
         print('Query cannot be empty. Please enter a valid query.')
         continue
-    result = pdf_qa(
-        {
-            "question": query, 
-            "chat_history": chat_history
-        }
-    )
+    # result = pdf_qa(
+    #     {
+    #         "question": query, 
+    #         "chat_history": chat_history
+    #     }
+    # )
+
+    result = qachain({"query": query})
+
     print(f"{white}Answer: " + result["answer"])
-    chat_history.append((query, result["answer"]))
+    # chat_history.append((query, result["answer"]))
